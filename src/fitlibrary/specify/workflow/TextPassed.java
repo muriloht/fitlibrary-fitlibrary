9acb2eda28961ce9e61fa53c8ea49417abc0cfb3
Rick Mugridge <rick@rimuresearch.com>
2010-08-18 11:30:05 +1200

    Added specs for strings with \n and tags being passed to action methods

---
+++ src/fitlibrary/specify/workflow/TextPassed.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2010 Rick Mugridge, www.RimuResearch.com
+ * Released under the terms of the GNU General Public License version 2 or later.
+*/
+
+package fitlibrary.specify.workflow;
+
+public class TextPassed {
+	public boolean isSeveralLines(String s) {
+		return s.equals("abc\ndef");
+	}
+	public boolean isSeveralLinesWithNewlines(String s) {
+		System.out.println("String is '"+s+"'");
+		return s.equals("\nabc\ndef\n");
+	}
+	public boolean hasTags(String s) {
+		return s.equals("<a>A<b/></a>");
+	}
+}
