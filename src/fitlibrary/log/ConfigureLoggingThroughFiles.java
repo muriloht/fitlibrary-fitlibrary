2f29c6a0cbf76230f858c949afe5c6528145bde6
Rick Mugridge <rick@rimuresearch.com>
2010-12-02 17:56:00 +1300

    Document build process. Allow for logging property files to be absent. Have explicit destination directory for final build step so it can be altered in Hudson to move it into win7 space.

--- src/fitlibrary/log/ConfigureLoggingThroughFiles.java
+++ src/fitlibrary/log/ConfigureLoggingThroughFiles.java
@@ -1,15 +1,23 @@
 package fitlibrary.log;
 
+import java.io.File;
+
 import org.apache.log4j.PropertyConfigurator;
 
 public class ConfigureLoggingThroughFiles {
 	public static void configure() {
 		configure("");
 	}
+
 	public static void configure(String diry) {
 		PropertyConfigurator propertyConfigurator = new PropertyConfigurator();
-		propertyConfigurator.doConfigure(diry+"FitLibraryLogger.properties", FitLibraryLogger.getOwnHierarchy());
-		propertyConfigurator.doConfigure(diry+"FixturingLogger.properties", FixturingLogger.getOwnHierarchy());
-		PropertyConfigurator.configure(diry+"log4j.properties");
+		if (new File(diry + "FitLibraryLogger.properties").exists())
+			propertyConfigurator.doConfigure(diry + "FitLibraryLogger.properties",
+					FitLibraryLogger.getOwnHierarchy());
+		if (new File(diry + "FixturingLogger.properties").exists())
+			propertyConfigurator.doConfigure(diry + "FixturingLogger.properties",
+					FixturingLogger.getOwnHierarchy());
+		if (new File(diry + "log4j.properties").exists())
+			PropertyConfigurator.configure(diry + "log4j.properties");
 	}
 }
